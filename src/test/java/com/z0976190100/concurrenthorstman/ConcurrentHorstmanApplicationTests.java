7c3289e947c7a674f2fc2883714b83fbe1e38c18
z0976190100 <z0976190100@gmail.com>
2018-08-30 03:43:14 +0300

    init commit

---
+++ src/test/java/com/z0976190100/concurrenthorstman/ConcurrentHorstmanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.z0976190100.concurrenthorstman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrentHorstmanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
